0fe4b4fbdf8715f9bd663a054185b192aac3ce63
Karixio <krzysztof678876@gmail.com>
2023-12-11 18:30:47 +0100

    general_structure_of_project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {MainComponent} from "./components/main/main.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'zm-project-front';
-}
+export class AppComponent {}
---
+++ src/app/components/shared/display-section/display-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-section.component.html',
+  styleUrl: './display-section.component.scss'
+})
+export class DisplaySectionComponent {
+
+}
